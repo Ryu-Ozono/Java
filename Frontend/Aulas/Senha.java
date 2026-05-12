9b2e0d3d186c4054e018688cc6622dc13c42273f
Ryu-Ozono <ozoprofissional@gmail.com>
2026-05-12 00:18:52 +0000

    commit

---
+++ Frontend/Aulas/Senha.java
@@ -0,0 +1,16 @@
+public class Senha{
+    public static void main(String[] args) {
+
+        int senha = 1234;
+        int tentativa = 0;
+        
+        while (tentativa != senha){
+            tentativa= 1234;
+            System.out.println("Acesso negado");
+
+            if (tentativa = senha);
+                System.out.println("Tentando acessar...");
+        }
+        System.out.println("Acesso liberado");
+    }
+}
